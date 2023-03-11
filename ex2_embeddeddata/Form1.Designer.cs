58ea2e0db236208b1abc36c9dee4684800eec77a
fil <pricketts@hotmail.com>
2023-03-11 13:49:24 +0000

    Added a project showing some sample data

--- ex2_embeddeddata/Form1.Designer.cs
+++ ex2_embeddeddata/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace rdlc_dotnet7
+﻿namespace ex2_embeddeddata
 {
     partial class Form1
     {
@@ -31,6 +31,9 @@
             label1 = new Label();
             panel1 = new Panel();
             reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
@@ -47,9 +50,9 @@
             // 
             panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             panel1.Controls.Add(reportViewer1);
-            panel1.Location = new Point(12, 27);
+            panel1.Location = new Point(12, 122);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1137, 482);
+            panel1.Size = new Size(1137, 387);
             panel1.TabIndex = 2;
             panel1.Paint += panel1_Paint;
             // 
@@ -59,14 +62,44 @@
             reportViewer1.Location = new Point(0, 0);
             reportViewer1.Name = "ReportViewer";
             reportViewer1.ServerReport.BearerToken = null;
-            reportViewer1.Size = new Size(1137, 482);
+            reportViewer1.Size = new Size(1137, 387);
             reportViewer1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 24);
+            label2.Name = "label2";
+            label2.Size = new Size(297, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Step 1. Create DataSet1 with the columns ID and NAME";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 39);
+            label3.Name = "label3";
+            label3.Size = new Size(583, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Step 2. Add a Table to the body of the report, link the table to DataSet1 and map the columns to ID and NAME";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 54);
+            label4.Name = "label4";
+            label4.Size = new Size(953, 15);
+            label4.TabIndex = 5;
+            label4.Text = "Step 3. In the Form load event create a DataTable with the columns ID and NAME then populate it with a few rows of data, and bind this DataTable into the DataSources of the report";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1161, 521);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(panel1);
             Controls.Add(label1);
             Name = "Form1";
@@ -82,5 +115,8 @@
         private Label label1;
         private Panel panel1;
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
---
+++ ex2_embeddeddata/Form1.cs
@@ -0,0 +1,116 @@
+using Microsoft.Reporting.Map.WebForms.BingMaps;
+using Microsoft.Reporting.WinForms;
+using System.Data;
+
+namespace ex2_embeddeddata
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private string FindResourceName(string filename)
+        {
+            var assembly = System.Reflection.Assembly.GetEntryAssembly();
+            var resourceNames = assembly.GetManifestResourceNames();
+
+            foreach (var res in resourceNames)
+            {
+                System.Diagnostics.Debug.Write(res);
+            }
+
+            return resourceNames.FirstOrDefault(x => x.EndsWith(filename));
+        }
+
+        private DataTable GetData()
+        {
+            // return some sample data
+
+            DataTable table = new DataTable();
+            table.Columns.Add(new DataColumn("ID"));
+            table.Columns.Add(new DataColumn("NAME"));
+
+            DataRow row = table.NewRow();
+            row["ID"] = 1;
+            row["NAME"] = "One";
+            table.Rows.Add(row);
+
+            row = table.NewRow();
+            row["ID"] = 2;
+            row["NAME"] = "Two";
+            table.Rows.Add(row);
+
+            row = table.NewRow();
+            row["ID"] = 3;
+            row["NAME"] = "Three";
+            table.Rows.Add(row);
+
+            row = table.NewRow();
+            row["ID"] = 4;
+            row["NAME"] = "Four";
+            table.Rows.Add(row);
+
+            row = table.NewRow();
+            row["ID"] = 5;
+            row["NAME"] = "Five";
+            table.Rows.Add(row);
+
+            row = table.NewRow();
+            row["ID"] = 6;
+            row["NAME"] = "Six";
+            table.Rows.Add(row);
+
+            row = table.NewRow();
+            row["ID"] = 7;
+            row["NAME"] = "Seven";
+            table.Rows.Add(row);
+
+            row = table.NewRow();
+            row["ID"] = 8;
+            row["NAME"] = "Eight";
+            table.Rows.Add(row);
+
+            row = table.NewRow();
+            row["ID"] = 9;
+            row["NAME"] = "Nine";
+            table.Rows.Add(row);
+
+            row = table.NewRow();
+            row["ID"] = 10;
+            row["NAME"] = "Ten";
+            table.Rows.Add(row);
+
+            return table;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var res = FindResourceName("Report2.rdlc");
+            var data = GetData();
+
+            reportViewer1.LocalReport.ReportEmbeddedResource = res;
+
+            // Note: LocalReport.DataSources is empty, but GetDataSourceNames returns the name of the design time DataSource
+            // the DataSource that we add to the report at runtime needs to have the same name
+
+            System.Diagnostics.Debug.WriteLine("Report DataSources:");
+            foreach (var ds in reportViewer1.LocalReport.GetDataSourceNames())
+            {
+                System.Diagnostics.Debug.WriteLine(ds);
+            }
+
+            reportViewer1.LocalReport.DataSources.Clear();
+            reportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WinForms.ReportDataSource("DataSet1", data));
+
+            reportViewer1.SetDisplayMode(Microsoft.Reporting.WinForms.DisplayMode.PrintLayout);
+            reportViewer1.RefreshReport();
+        }
+    }
+}
